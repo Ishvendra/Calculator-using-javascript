dbd522348b853f593fe7afbb8023e761ef3b9c79
Ishvendra Singh <71009922+Ishvendra@users.noreply.github.com>
2021-06-07 19:17:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,10 +73,43 @@ for(var i=0; i<button.length;i++){
             operand2 = null;
             operator = null;
         }
+        else if(value == 'theme'){
+            let x = display.innerHTML;
+            display.innerHTML = x;
+        }
         else{
             display.innerHTML += value;
         }
         
     });
+}
+var r = document.querySelector(':root');
+var bs = document.querySelector('.container');
+var cnt = 0;
+
+function togglet(){
+
+    cnt++;
+    if(cnt == 3){
+        cnt = 0;
+    }
+    if(cnt == 0){
+        r.style.setProperty("--bg" , "#F1D1B5");
+        r.style.setProperty("--keys" , "#305F72");
+        r.style.setProperty("--buttons" , "#000");
+        bs.style.boxShadow = "0";
+    }
+    else if(cnt == 1){
+        r.style.setProperty("--bg" , "#222222");
+        r.style.setProperty("--keys" , "#000");
+        r.style.setProperty("--buttons" , "#fff");
+        bs.style.boxShadow = "10px 10px 10px black";
+    }
+    else{
+        r.style.setProperty("--bg" , "#58A6A6");
+        r.style.setProperty("--keys" , "#421E22");
+        r.style.setProperty("--buttons" , "#fff");
+        bs.style.boxShadow = "0px 0px 0px black";
+    }
 }
 
